7a5dee67a25893328686666b83eb27768ab65631
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2020-12-02 10:25:28 -0800

    Adding in Awesome widget

---
+++ src/components/AwesomeWidget.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AwesomeWidget = ({count}) => {
+    return (
+        <div>
+            <p> This is your count: {}</p>
+        </div>
+    )
+}
+
+export default AwesomeWidget
